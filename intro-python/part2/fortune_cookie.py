a86bc1a0e19781abec29b142e956d04c702f3cc4
Polo <nathan.rx@gmail.com>
2022-01-13 23:56:59 -0600

    my solution

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -34,8 +34,8 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
     # cookie's message.
-
-    raise NotImplementedError()
+    generate_fortune()
+    return generate_lucky_numbers(how_many_lucky_numbers)
 
 
 def main():
